effe902dfde5f3eb5b37419bead0538187cbacd0
taitongngocanh <taitongngocanh@gmail.com>
2025-08-19 14:59:59 +0700

    add carousel into homepage

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBook } from './layouts/HomePage/ExploreTopBook';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploreTopBook/>
+      <Carousel/>
     </div>
   );
 }
---
+++ 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -0,0 +1,101 @@
+export const Carousel = () => {
+     return (
+          <div className="container mt-5" style={{ height: 550 }}>
+               <div className="homepage-carousel-title">
+                    <h3>次の「夢中になって夜更かししちゃう本」を見つけよう.</h3>
+               </div>
+
+               <div id="carouselExampleControls" className="carousel carousel-dark slide mt-5 d-none d-lg-block"
+                    data-bs-interval="false">
+
+                    {/* Desktop */}
+                    <div className="carousel-inner">
+                         <div className="carousel-item active">
+                              <div className="row d-flex justify-content-center align-items-center">
+                                   <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                                        <div className="text-center">
+                                             <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                                  width="151" height="233"
+                                                  alt="book" />
+
+                                             <h6 className="mt-2">本</h6>
+                                             <p>プログラミングが大好き.</p>
+                                             <a href="#" className="btn main-color text-white reserve-btn">予約</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div className="carousel-item">
+                              <div className="row d-flex justify-content-center align-items-center">
+                                   <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                                        <div className="text-center">
+                                             <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                                  width="151" height="233"
+                                                  alt="book" />
+
+                                             <h6 className="mt-2">本</h6>
+                                             <p>プログラミングが大好き.</p>
+                                             <a href="#" className="btn main-color text-white reserve-btn">予約</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div className="carousel-item">
+                              <div className="row d-flex justify-content-center align-items-center">
+                                   <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                                        <div className="text-center">
+                                             <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                                  width="151"
+                                                  height="233"
+                                                  alt="book" />
+
+                                             <h6 className="mt-2">本</h6>
+                                             <p>プログラミングが大好き.</p>
+                                             <a href="#" className="btn main-color text-white reserve-btn">予約</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                    </div>
+
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                         data-bs-slide="prev">
+                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                         <span className="visually-hidden">Previous</span>
+                    </button>
+
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                         data-bs-slide="next">
+                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                         <span className="visually-hidden">Next</span>
+                    </button>
+               </div>
+
+               {/* Mobile */}
+               <div className="d-lg-none mt-3">
+                    <div className="row d-flex justify-content-center align-items-center">
+                         <div className="text-center">
+                              <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                   alt="book"
+                                   width="151"
+                                   height="233" />
+
+                              <h6 className="mt-2">
+                                   <b>本</b>
+                              </h6>
+                              <p>プログラミングが大好き.</p>
+                              <a className="btn main-color text-white reserve-btn" href="#">Reserve</a>
+                         </div>
+                    </div>
+               </div>
+
+               <div className="homepage-carousel-title mt-3">
+                    <a className="btn btn-outline-secondary btn-lg" href="#">もっと見る</a>
+               </div>
+
+          </div>
+     );
+}
