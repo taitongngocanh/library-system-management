a70e8b8e0551b10a7dd699129596a09d85cc0ecc
taitongngocanh <taitongngocanh@gmail.com>
2025-08-19 14:21:48 +0700

    fix explore book component

--- 03-frontend/react-library/src/layouts/HomePage/ExploreTopBook.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/ExploreTopBook.tsx
@@ -6,7 +6,7 @@ export const ExploreTopBook = () => {
                     <div>
                          <h1 className="display-5 fw-bold">次の冒険を見つけよう</h1>
                          <p className="col-md-8 fs-4">次はどこへ行きたいですか？</p>
-                         <a className="btn main-color btn-lg text-white" href="#" type="button">人気の本を探そう</a>
+                         <a className="btn main-color btn-lg text-white btn-explore" href="#" type="button">人気の本を探そう</a>
                     </div>
                </div>
           </div>
