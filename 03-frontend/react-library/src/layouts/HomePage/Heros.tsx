202ac8bb2c9742a8de00a5838161f48ca431e145
taitongngocanh <taitongngocanh@gmail.com>
2025-08-20 14:56:43 +0700

    fix mobile content

--- 03-frontend/react-library/src/layouts/HomePage/Heros.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/Heros.tsx
@@ -41,25 +41,22 @@ export const Heros = () => {
                          <div className="m-2">
                               <div className="col-image-left"></div>
                               <div className="mt-2">
-                                   <h1>What have you been reading</h1>
+                                   <h1>最近、何を読んでいますか</h1>
                                    <p className="lead">
-                                        The library team would love to know what you have been reading.
-                                        Whether it is to learn a new skill or grow within one,
-                                        we will be able to provide the top content for you!
+                                        図書館チームは、あなたが読んでいる本を知りたいと思っています。
+                                        学びにも成長にも役立つ最高のコンテンツをご用意します！
                                    </p>
-                                   <a href="#" className="btn main-color btn-lg text-white">Sign up</a>
+                                   <a href="#" className="btn main-color btn-lg text-white reserve-btn">新規登録</a>
                               </div>
                          </div>
 
                          <div className="m-2">
                               <div className="col-image"></div>
                               <div className="mt-2">
-                                   <h1>Our collection is aways changing!</h1>
+                                   <h1>蔵書はいつも新しくなっています！</h1>
                                    <p className="lead">
-                                        Try to check n daily as our collection is always changing!
-                                        We work nonstop to provide the most accurate book selecction possible
-                                        for our Luv 2 Read students! We are diligent about our book selection
-                                        and our books are always going to be our top priority
+                                        毎日チェック！蔵書は常に新しく。
+                                        東京大学の学生のために、正確で最適な本を厳選しています。
                                    </p>
                               </div>
                          </div>
