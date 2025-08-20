a996c86049490892d10cdbbf520e652dc2780543
taitongngocanh <taitongngocanh@gmail.com>
2025-08-20 14:07:26 +0700

    fix carousel

--- 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -5,19 +5,16 @@ export const Carousel = () => {
                     <h3>次の「夢中になって夜更かししちゃう本」を見つけよう.</h3>
                </div>
 
-               <div id="carouselExampleControls" className="carousel carousel-dark slide mt-5 d-none d-lg-block"
-                    data-bs-interval="false">
-
-                    {/* Desktop */}
+               {/* Start: Desktop */}
+               <div id="carouselExampleControls"
+                    className="carousel carousel-dark slide mt-5 d-none d-lg-block" data-bs-interval="false">
                     <div className="carousel-inner">
                          <div className="carousel-item active">
                               <div className="row d-flex justify-content-center align-items-center">
                                    <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
                                         <div className="text-center">
                                              <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                                  width="151" height="233"
-                                                  alt="book" />
-
+                                                  alt="book" width="151" height="233" />
                                              <h6 className="mt-2">本</h6>
                                              <p>プログラミングが大好き.</p>
                                              <a href="#" className="btn main-color text-white reserve-btn">予約</a>
@@ -26,14 +23,12 @@ export const Carousel = () => {
                               </div>
                          </div>
 
-                         <div className="carousel-item">
+                         <div className="carousel-item active">
                               <div className="row d-flex justify-content-center align-items-center">
                                    <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
                                         <div className="text-center">
                                              <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                                  width="151" height="233"
-                                                  alt="book" />
-
+                                                  alt="book" width="151" height="233" />
                                              <h6 className="mt-2">本</h6>
                                              <p>プログラミングが大好き.</p>
                                              <a href="#" className="btn main-color text-white reserve-btn">予約</a>
@@ -42,15 +37,12 @@ export const Carousel = () => {
                               </div>
                          </div>
 
-                         <div className="carousel-item">
+                         <div className="carousel-item active">
                               <div className="row d-flex justify-content-center align-items-center">
                                    <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
                                         <div className="text-center">
                                              <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                                  width="151"
-                                                  height="233"
-                                                  alt="book" />
-
+                                                  alt="book" width="151" height="233" />
                                              <h6 className="mt-2">本</h6>
                                              <p>プログラミングが大好き.</p>
                                              <a href="#" className="btn main-color text-white reserve-btn">予約</a>
@@ -58,44 +50,34 @@ export const Carousel = () => {
                                    </div>
                               </div>
                          </div>
-
                     </div>
 
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                    <div className="carousel-control-prev" typeof="button" data-bs-target="#carouselExampleControls"
                          data-bs-slide="prev">
                          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                          <span className="visually-hidden">Previous</span>
-                    </button>
+                    </div>
 
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                    <div className="carousel-control-next" typeof="button" data-bs-target="#carouselExampleControls"
                          data-bs-slide="next">
                          <span className="carousel-control-next-icon" aria-hidden="true"></span>
                          <span className="visually-hidden">Next</span>
-                    </button>
+                    </div>
                </div>
+               {/* End: Desktop */}
 
-               {/* Mobile */}
+               {/* Start: Mobile */}
                <div className="d-lg-none mt-3">
                     <div className="row d-flex justify-content-center align-items-center">
                          <div className="text-center">
-                              <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                   alt="book"
-                                   width="151"
-                                   height="233" />
-
-                              <h6 className="mt-2">
-                                   <b>本</b>
-                              </h6>
+                              <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} alt="book" width="151" height="233" />
+                              <h6 className="mt-2"><b>本</b></h6>
                               <p>プログラミングが大好き.</p>
-                              <a className="btn main-color text-white reserve-btn" href="#">Reserve</a>
+                              <a className="btn main-color text-white reserve-btn" href="#">予約</a>
                          </div>
                     </div>
                </div>
-
-               <div className="homepage-carousel-title mt-3">
-                    <a className="btn btn-outline-secondary btn-lg" href="#">もっと見る</a>
-               </div>
-
+               {/* End: Mobile */}
           </div>
      );
 }
