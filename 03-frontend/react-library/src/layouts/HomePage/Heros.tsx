b677dc4a68837b4374f4b4fcfb61a988cbf13ce0
taitongngocanh <taitongngocanh@gmail.com>
2025-08-20 14:55:36 +0700

    change the content of heros to japanese

--- 03-frontend/react-library/src/layouts/HomePage/Heros.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/Heros.tsx
@@ -8,13 +8,12 @@ export const Heros = () => {
                          </div>
                          <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
                               <div className="ml-2">
-                                   <h1>What have you been reading</h1>
+                                   <h1>最近、何を読んでいますか</h1>
                                    <p className="lead">
-                                        The library team would love to know what you have been reading.
-                                        Whether it is to learn a new skill or grow within one,
-                                        we will be able to provide the top content for you!
+                                        図書館チームは、あなたが読んでいる本を知りたいと思っています。
+                                        学びにも成長にも役立つ最高のコンテンツをご用意します！
                                    </p>
-                                   <a href="#" className="btn main-color btn-lg text-white">Sign up</a>
+                                   <a href="#" className="btn main-color btn-lg text-white reserve-btn">新規登録</a>
                               </div>
                          </div>
                     </div>
@@ -22,12 +21,10 @@ export const Heros = () => {
                     <div className="row g-0">
                          <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
                               <div className="ml-2">
-                                   <h1>Our collection is always changing!</h1>
+                                   <h1>蔵書はいつも新しくなっています！</h1>
                                    <p className="lead">
-                                        Try to check n daily as our collection is always changing!
-                                        We work nonstop to provide the most accurate book selecction possible
-                                        for our Luv 2 Read students! We are diligent about our book selection
-                                        and our books are always going to be our top priority
+                                        毎日チェック！蔵書は常に新しく。
+                                        東京大学の学生のために、正確で最適な本を厳選しています。
                                    </p>
                               </div>
                          </div>
