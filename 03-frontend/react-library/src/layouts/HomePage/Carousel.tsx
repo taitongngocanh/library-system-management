fee29c1843bc933c194f1412e65452550d44799a
taitongngocanh <taitongngocanh@gmail.com>
2025-08-20 14:21:35 +0700

    Add ReturnBook component

--- 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
      return (
           <div className="container mt-5" style={{ height: 550 }}>
@@ -11,43 +13,25 @@ export const Carousel = () => {
                     <div className="carousel-inner">
                          <div className="carousel-item active">
                               <div className="row d-flex justify-content-center align-items-center">
-                                   <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                        <div className="text-center">
-                                             <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                                  alt="book" width="151" height="233" />
-                                             <h6 className="mt-2">本</h6>
-                                             <p>プログラミングが大好き.</p>
-                                             <a href="#" className="btn main-color text-white reserve-btn">予約</a>
-                                        </div>
-                                   </div>
+                                   <ReturnBook />
+                                   <ReturnBook />
+                                   <ReturnBook />
                               </div>
                          </div>
 
                          <div className="carousel-item active">
                               <div className="row d-flex justify-content-center align-items-center">
-                                   <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                        <div className="text-center">
-                                             <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                                  alt="book" width="151" height="233" />
-                                             <h6 className="mt-2">本</h6>
-                                             <p>プログラミングが大好き.</p>
-                                             <a href="#" className="btn main-color text-white reserve-btn">予約</a>
-                                        </div>
-                                   </div>
+                                   <ReturnBook />
+                                   <ReturnBook />
+                                   <ReturnBook />
                               </div>
                          </div>
 
                          <div className="carousel-item active">
                               <div className="row d-flex justify-content-center align-items-center">
-                                   <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                        <div className="text-center">
-                                             <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                                  alt="book" width="151" height="233" />
-                                             <h6 className="mt-2">本</h6>
-                                             <p>プログラミングが大好き.</p>
-                                             <a href="#" className="btn main-color text-white reserve-btn">予約</a>
-                                        </div>
-                                   </div>
+                                   <ReturnBook />
+                                   <ReturnBook />
+                                   <ReturnBook />
                               </div>
                          </div>
                     </div>
@@ -69,12 +53,7 @@ export const Carousel = () => {
                {/* Start: Mobile */}
                <div className="d-lg-none mt-3">
                     <div className="row d-flex justify-content-center align-items-center">
-                         <div className="text-center">
-                              <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} alt="book" width="151" height="233" />
-                              <h6 className="mt-2"><b>本</b></h6>
-                              <p>プログラミングが大好き.</p>
-                              <a className="btn main-color text-white reserve-btn" href="#">予約</a>
-                         </div>
+                         <ReturnBook />
                     </div>
                </div>
                {/* End: Mobile */}
---
+++ 03-frontend/react-library/src/layouts/HomePage/ReturnBook.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const ReturnBook = () => {
+     return (
+          <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+               <div className="text-center">
+                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                         alt="book" width="151" height="233" />
+                    <h6 className="mt-2">本</h6>
+                    <p>プログラミングが大好き.</p>
+                    <a href="#" className="btn main-color text-white reserve-btn">予約</a>
+               </div>
+          </div>
+     );
+
+}
