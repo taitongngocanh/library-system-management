8d9235387743b1dfc858cb132c6159b792d57299
taitongngocanh <taitongngocanh@gmail.com>
2025-09-04 15:23:30 +0700

    add useEffect for BookChecoutPage component

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,15 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const BookCheckoutPage = () => {
 
      const [book, setBook] = useState<BookModel>();
-     const [isLoadingBook, setIsLoadingBook] = useState(true);
+     const [isLoading, setIsLoading] = useState(true);
      const [httpError, setHttpError] = useState(null);
 
      const bookId = (window.location.pathname).split('/')[2];
 
-     return(
+     useEffect(() => {
+          const fetchBook = async () => {
+               const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+               const response = await fetch(baseUrl);
+
+               if (!response.ok) {
+                    throw new Error("Something went wrong!")
+               }
+               
+               const responseJson = await response.json();
+
+               const loadedBook: BookModel = {
+                    id: responseJson.id,
+                    title: responseJson.title,
+                    author: responseJson.author,
+                    description: responseJson.description,
+                    copies: responseJson.copies,
+                    copiesAvailable: responseJson.copiesAvailable,
+                    category: responseJson.category,
+                    img: responseJson.img
+               };
+
+               setBook(loadedBook);
+               setIsLoading(false);
+
+          };
+
+          fetchBook().catch((error: any) => {
+               setIsLoading(false);
+               setHttpError(error.message);
+          })
+     }, []);
+
+     if (isLoading) {
+          return (
+               <SpinnerLoading />
+          )
+     }
+
+     if (httpError) {
+          return (
+               <div className="container mt-5">
+                    <p>{httpError}</p>
+               </div>
+          )
+     }
+
+     return (
           <div>
                <h3>Hi World</h3>
           </div>
