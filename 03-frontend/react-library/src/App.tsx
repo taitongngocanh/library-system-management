31041e2aba337585d8b892a810384d9e0357aa89
taitongngocanh <taitongngocanh@gmail.com>
2025-09-04 14:15:04 +0700

    Add exact for path /, Redirect

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,22 +4,28 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Route path='/'>
-        <HomePage/>
-      </Route>
-      
-      <Route path='/search'>
-        <SearchBooksPage/>
-      </Route>
-      
-      <Footer/>
+      <Navbar />
 
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home'/>
+        </Route>
+
+        <Route path='/home'>
+          <HomePage/>
+        </Route>
+
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
+
+      <Footer />
     </div>
   );
 }
