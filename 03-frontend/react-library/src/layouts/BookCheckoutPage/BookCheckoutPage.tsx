6f873b2957d77b53b6c0a70d54458b99f33d50be
taitongngocanh <taitongngocanh@gmail.com>
2025-09-04 15:44:42 +0700

    css for BookCheckoutPage

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -19,7 +19,7 @@ export const BookCheckoutPage = () => {
                if (!response.ok) {
                     throw new Error("Something went wrong!")
                }
-               
+
                const responseJson = await response.json();
 
                const loadedBook: BookModel = {
@@ -60,7 +60,69 @@ export const BookCheckoutPage = () => {
 
      return (
           <div>
-               <h3>Hi World</h3>
+
+               <div className="container d-none d-lg-block">
+                    <div className="row mt-5">
+                         <div className="col-sm-2 col-md-2">
+                              {
+                                   book?.img ?
+                                        <img src={book?.img} width='226' height='349' alt="Book" />
+                                        :
+                                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                             width='226' height='349' alt="Book" />
+                              }
+                         </div>
+
+                         <div className="col-4 col-md-4 container">
+                              <div className="ml-2">
+                                   <h2>
+                                        {book?.title}
+                                   </h2>
+
+                                   <h5 className="text-primary">
+                                        {book?.author}
+                                   </h5>
+
+                                   <p className="lead">
+                                        {book?.description}
+                                   </p>
+
+                              </div>
+                         </div>
+                    </div>
+                    <hr />
+                    </div>
+
+
+
+
+                    <div className="container d-lg-none mt-5">
+                         <div className="d-flex justify-content-center align-items-center">
+                              {
+                                   book?.img ?
+                                        <img src={book?.img} width='226' height='349' alt="Book" />
+                                        :
+                                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                             width='226' height='349' alt="Book" />
+                              }
+                         </div>
+
+                         <div className="mt-4">
+                              <div className="ml-2">
+                                   <h2>{book?.title}</h2>
+                                   <h5 className="text-primary">
+                                        {book?.author}
+                                   </h5>
+                                   <p className="lead">
+                                        {book?.description}
+                                   </p>
+                              </div>
+                         </div>
+
+                         <hr />
+
+                    </div>
+               
           </div>
      );
 }
